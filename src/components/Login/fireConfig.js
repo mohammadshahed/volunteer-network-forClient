7a1d5e14a9423b7cc8dc6814a86f95376384fda6
mohammadshahed <silentshahed@gmail.com>
2020-10-11 13:13:54 +0600

    first commit

---
+++ src/components/Login/fireConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-NUzkCVlCMOv0dLkwzj7saGxOcqaxgvk",
+    authDomain: "volunteer-networks-bd.firebaseapp.com",
+    databaseURL: "https://volunteer-networks-bd.firebaseio.com",
+    projectId: "volunteer-networks-bd",
+    storageBucket: "volunteer-networks-bd.appspot.com",
+    messagingSenderId: "967673030218",
+    appId: "1:967673030218:web:591b704fe64288e10128b3"
+  };
+
+export default firebaseConfig
